a4bce6f433342ab29c45ac9a07f1ccdd9e486b81
aomsinon1 <156872565+aomsinon1@users.noreply.github.com>
2024-03-20 11:56:22 +0700

    Update 3_☠︎☠︎DataAndChart.py

--- pages/3_☠︎☠︎DataAndChart.py
+++ pages/3_☠︎☠︎DataAndChart.py
@@ -3,16 +3,10 @@ import pandas as pd
 
 st.header("Show Data Index Price")
 
-df = pd.read_csv("./data/dummy_data.csv")
+df=pd.read_csv("./data/dummy_data.csv")
 st.write(df.head(10))
 
 st.header("Show Chart")
 
-# Define data_for_interest, colors, and interest
-data_for_interest = df  # ให้ data_for_interest เป็น DataFrame ที่ต้องการแสดงในกราฟ
-colors = ['#FF0000', '#00FF00', '#0000FF']  # กำหนดสีของเส้นกราฟ
-interest = "interest_1"  # กำหนดชื่อสำหรับกราฟ
-
-# Plot line chart
 st.line_chart(
-   data_for_interest, x="platform", y="age", color=colors[0], use_container_width=True, key=interest)
+   data_for_interest, x="platform", y="age", color=colors[i], use_container_width=True, key=interest)

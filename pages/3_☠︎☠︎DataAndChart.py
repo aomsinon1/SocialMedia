d97e9838fc85b1e2033f4cf01a92d96f9772a736
aomsinon1 <156872565+aomsinon1@users.noreply.github.com>
2024-03-20 12:44:47 +0700

    Update 3_☠︎☠︎DataAndChart.py

--- pages/3_☠︎☠︎DataAndChart.py
+++ pages/3_☠︎☠︎DataAndChart.py
@@ -13,7 +13,6 @@ st.header("Show Chart")
 age_range = st.slider("Select age range", min_value=df["age"].min(), max_value=df["age"].max(), value=(df["age"].min(), df["age"].max()))
 filtered_df = df[(df["age"] >= age_range[0]) & (df["age"] <= age_range[1])]
 
-# สร้างกราฟแท่ง
 st.bar_chart(filtered_df.set_index('platform')['age'])
 
 

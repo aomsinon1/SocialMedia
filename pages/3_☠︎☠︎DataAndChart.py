1590d1d23ec223dc211124e110ec19697811c06b
aomsinon1 <156872565+aomsinon1@users.noreply.github.com>
2024-03-20 12:27:13 +0700

    Update 3_☠︎☠︎DataAndChart.py

--- pages/3_☠︎☠︎DataAndChart.py
+++ pages/3_☠︎☠︎DataAndChart.py
@@ -21,13 +21,13 @@ st.header("Bar Chart")
 # สร้างช่องสไลเดอร์สำหรับการเลือกช่วงอายุ
 min_age = df["age"].min()
 max_age = df["age"].max()
-age_range = st.slider("Select age range", min_value=min_age, max_value=max_age, value=(min_age, max_age))
+age_range = st.slider("Select age range", min_value=min_age, max_value=max_age)
 
 # กรองข้อมูลตามช่วงอายุที่ผู้ใช้เลือก
 filtered_df = df[(df["age"] >= age_range[0]) & (df["age"] <= age_range[1])]
 
 # แบ่งข้อมูลตามช่วงอายุและนับจำนวนบุคคลในแต่ละช่วงอายุ
-age_groups = pd.cut(filtered_df["age"], bins=range(age_range[0], age_range[1]+1, 10))
+age_groups = pd.cut(filtered_df["age"], bins=range(int(age_range[0]), int(age_range[1])+1, 10))
 age_counts = age_groups.value_counts().sort_index()
 
 # สร้างกราฟแท่งโดยให้แกน x เป็นช่วงอายุและแกน y เป็นจำนวนของบุคคลในแต่ละช่วงอายุ

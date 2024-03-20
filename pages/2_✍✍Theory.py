6769c58179ae4b88dda025c1d0491a2db1331b56
aomsinon1 <156872565+aomsinon1@users.noreply.github.com>
2024-03-20 11:13:01 +0700

    .

---
+++ pages/2_✍✍Theory.py
@@ -0,0 +1,28 @@
+import json
+import time
+import requests
+import streamlit as st
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+lottie_url_hello = "https://lottie.host/04a73bbb-ad0e-4cf6-8200-7aae7ccc332f/oOspXtZYfI.json"
+lottie_hello = load_lottieurl(lottie_url_hello)
+st_lottie(lottie_hello, key="hello")
+
+st.header("ทฤษฎีที่เกี่ยวข้อง")
+
+st.subheader("1.ทฤษฎีเหมืองข้อมูล")
+st.info("""
+xxxxxxxxxx
+""")
+
+st.subheader("2.เทคนิคการการทำจินตทัศน์ข้อมูล")
+st.info("""
+xxxxxxxxxx
+""")
---
+++ pages/3_☠︎☠︎DataAndChart.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+st.header("Show Data Index Price")
+
+df=pd.read_csv("./data/2022worldcup.csv")
+st.write(df.head(10))
+
+st.header("Show Chart")
+
+st.line_chart(
+   df, x="age", y=["'goals_overall", "unemployment_rate"], color=["#FF0000", "#0000FF"]  # Optional
+)

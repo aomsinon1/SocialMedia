c54f15e1adb1418bc14933cf1c37805605cdf460
aomsinon1 <156872565+aomsinon1@users.noreply.github.com>
2024-03-21 21:48:01 +0700

    Update SM.py

--- SM.py
+++ SM.py
@@ -5,12 +5,17 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 from streamlit_lottie import st_lottie_spinner
 
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
+lottie_url_hello = "https://lottie.host/e1e288b5-1a12-470f-9f29-45e1171dc7e8/Way5aKuCFU.json"
+lottie_hello = load_lottieurl(lottie_url_hello)
 
-# ถ้ามีการอัปโหลดไฟล์ GIF
-if uploaded_file is not None:
-    # แสดงรูปภาพ GIF
-    st.image(uploaded_file, caption='https://media1.tenor.com/m/_3FJpcm3xgwAAAAd/fun-pet-pet-fun.gif', use_column_width=True)
+st.header("การพยากรณ์ข้อมูลการใช้platformด้วยเทคนิค Linear Regression☠︎")
+st.header("by A0MS1N")
 
 
 #การเรียกใช้งาาน lottie file
